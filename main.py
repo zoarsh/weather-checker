778e7e327b0906abc0f1cf938208d77beae7e5e5
zoarsh <zoarsharvit@gmail.com>
2025-08-03 13:46:13 +0300

    project setup
    Includes config file, API call, and weather printout.

---
+++ main.py
@@ -0,0 +1,34 @@
+
+import json
+import requests
+
+# Welcome message when lunching the Weather Checker app
+print("Hello from Weather Checker!")
+
+# Reading API key from the external config file (config.json)
+with open("config.json") as f:
+    config = json.load(f)
+
+# Storing the API key for future use
+API_KEY = config["api_key"]
+
+while True:
+    user_city = input("Please enter your city name:\n")
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={user_city.lower()}&appid={API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        break
+    else:
+        print("City not found! Please try again\n", user_city)
+
+data = response.json()
+city = data ["name"]
+country = data["sys"]["country"]
+description = data["weather"][0]["description"]
+temp = data["main"]["temp"]
+temp_celsius = round(temp - 273.15, 1)
+humidity = data["main"]["humidity"]
+print(f"Weather in {city}, {country}:\n{description}\nTemperature:{temp_celsius}\u00B0C\nHumidity:{humidity}%")
+
+
